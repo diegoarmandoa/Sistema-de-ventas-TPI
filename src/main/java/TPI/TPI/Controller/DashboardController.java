d78f64f67e4eb4d38e201d3de0291f0ff1314cb3
Diego Mejia <pcdiego17@gmail.com>
2022-12-16 21:15:35 -0600

    añadiendo cambios minimos

--- src/main/java/TPI/TPI/Controller/DashboardController.java
+++ src/main/java/TPI/TPI/Controller/DashboardController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 
-public class DashboardIndexController {
+public class DashboardController {
 
     @RequestMapping("/dashboard/home")
     public String inicio(){
