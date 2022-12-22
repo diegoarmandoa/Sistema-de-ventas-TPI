e59af155adf6839edf581c8b8ed94e335cb2405a
Diego Mejia <pcdiego17@gmail.com>
2022-12-22 11:53:52 -0600

    añadiendo persistencia a persona

--- src/main/java/TPI/TPI/Config/SecurityConfiguration.java
+++ src/main/java/TPI/TPI/Config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import TPI.TPI.service.api.UsuarioServiceAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,7 +19,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+	@Lazy
 	@Autowired
 	private UsuarioServiceAPI userServiceAPI;
 
