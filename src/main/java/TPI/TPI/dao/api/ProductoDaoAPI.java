cd9b68f4346f1c9b24ff50dc40c72a80c8f97127
Alejandro <105890159+Deras071@users.noreply.github.com>
2022-12-29 18:22:10 -0600

    Vista principal

--- src/main/java/TPI/TPI/dao/api/ProductoDaoAPI.java
+++ src/main/java/TPI/TPI/dao/api/ProductoDaoAPI.java
@@ -1,7 +1,6 @@
 package TPI.TPI.dao.api;
 
 import TPI.TPI.Entity.Productos;
-import org.apache.commons.math3.stat.descriptive.summary.Product;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductoDaoAPI extends CrudRepository<Productos, Integer> {
